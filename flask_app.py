9c935cfcf7e542d9b84d09e5a5e467e21e56374c
mokuq <imoe.for@gmail.com>
2019-07-24 16:40:41 +0300

    added inline buttons

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,16 @@
 # it is a bot with a webhook
 import telebot # pip install pyTelegramBotAPI
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import urllib
 import json
 import datetime
-from bottoken import TOKEN, SECRET
+from config import TOKEN, SECRET, URL
 from flask import Flask, request, abort
 
 
 bot = telebot.TeleBot(TOKEN, threaded=False)
 bot.remove_webhook()
-url = "https://vmokuq.pythonanywhere.com/{}".format(SECRET)
+url = URL + SECRET
 bot.set_webhook(url = url)
 
 app = Flask(__name__)
@@ -25,9 +26,16 @@ def telegram_webhook():
     else:
         abort(403)
 
+listofcurrencies = ["AUD", "CAD", "CNY", "HRK", "CZK", "DKK", "HKD", "HUF", "INR", "IDR", "IRR", "ILS", "JPY", "KZT", "KRW", "MXN", "MDL", "NZD", "NOK", "RUB", "SAR", "SGD", "ZAR", "SEK", "CHF", "EGP", "GBP", "USD", "BYN", "AZN", "RON", "TRY", "XDR", "BGN", "EUR", "PLN", "DZD", "BDT", "AMD", "IQD", "KGS", "LBP", "LYD", "MYR", "MAD", "PKR", "VND", "THB", "AED", "TND", "UZS", "TWD", "TMT", "GHS", "RSD", "TJS", "GEL", "XAU", "XAG", "XPT", "XPD"]
+lstcur = '''List of currencies: \n/AUD\n /CAD\n /CNY\n /HRK\n /CZK\n /DKK\n /HKD\n /HUF\n /INR\n /IDR\n /IRR\n /ILS\n /JPY\n /KZT\n /KRW\n /MXN\n /MDL\n /NZD\n /NOK\n /RUB\n /SAR\n /SGD\n /ZAR\n /SEK\n /CHF\n /EGP\n /GBP\n /USD\n /BYN\n /AZN\n /RON\n /TRY\n /XDR\n /BGN\n /EUR\n /PLN\n /DZD\n /BDT\n /AMD\n /IQD\n /KGS\n /LBP\n /LYD\n /MYR\n /MAD\n /PKR\n /VND\n /THB\n /AED\n /TND\n /UZS\n /TWD\n /TMT\n /GHS\n /RSD\n /TJS\n /GEL\n /XAU\n /XAG\n /XPT\n /XPD'''
+
+
+def stat(id):
+    with open("stat.txt", "a") as file:
+        file.write(str(id)+","+ datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"\n")
+
 def get_rate(currency):
 	currencyname = currency.replace("/", "").upper()
-	listofcurrencies = ["AUD", "CAD", "CNY", "HRK", "CZK", "DKK", "HKD", "HUF", "INR", "IDR", "IRR", "ILS", "JPY", "KZT", "KRW", "MXN", "MDL", "NZD", "NOK", "RUB", "SAR", "SGD", "ZAR", "SEK", "CHF", "EGP", "GBP", "USD", "BYN", "AZN", "RON", "TRY", "XDR", "BGN", "EUR", "PLN", "DZD", "BDT", "AMD", "IQD", "KGS", "LBP", "LYD", "MYR", "MAD", "PKR", "VND", "THB", "AED", "TND", "UZS", "TWD", "TMT", "GHS", "RSD", "TJS", "GEL", "XAU", "XAG", "XPT", "XPD"]
 	if currencyname not in listofcurrencies:
 	    answer =  "Please, specify a currency name in a right way: \n /USD or \n /EUR  or \n USD, EUR \n etc."
 	    return answer
@@ -37,41 +45,51 @@ def get_rate(currency):
 	URL= 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=' + currencyname + '&date=' + todaydate + '&json'
 	content = urllib.request.urlopen(URL)
 	try:
-		answer = f"NBU rate {currencyname} / HRN =  {json.load(content)[0]['rate']} for today ({todaydate})"
+		answer = f"NBU rate {currencyname} / UAH =  {json.load(content)[0]['rate']} for today ({todaydate})"
 	except IndexError:
-		answer = "Please, specify a currency name in a right way: \n /USD or \n /EUR  or \n USD, EUR \n etc"
+		answer = "Something went wrong. Please try againe later"
 	return answer
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add( InlineKeyboardButton("USD", callback_data="USD"),
+                InlineKeyboardButton("EUR", callback_data="EUR"),
+                InlineKeyboardButton("other", callback_data="other"))
+    return markup
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    answer = message.chat.first_name + ", good day! Please select a currency: \n /USD \n /EUR \n etc.\n Full list of available currencies: /list"
-    bot.send_message(message.chat.id, answer)
+    bot.send_message(message.chat.id, "Please, select a  currency", reply_markup=gen_markup())
 
 @bot.message_handler(commands=['list'])
 def send_list(message):
-    answer = '''List of currencies: \n/AUD\n /CAD\n /CNY\n /HRK\n /CZK\n /DKK\n /HKD\n /HUF\n /INR\n /IDR\n /IRR\n /ILS\n /JPY\n /KZT\n /KRW\n /MXN\n /MDL\n /NZD\n /NOK\n /RUB\n /SAR\n /SGD\n /ZAR\n /SEK\n /CHF\n /EGP\n /GBP\n /USD\n /BYN\n /AZN\n /RON\n /TRY\n /XDR\n /BGN\n /EUR\n /PLN\n /DZD\n /BDT\n /AMD\n /IQD\n /KGS\n /LBP\n /LYD\n /MYR\n /MAD\n /PKR\n /VND\n /THB\n /AED\n /TND\n /UZS\n /TWD\n /TMT\n /GHS\n /RSD\n /TJS\n /GEL\n /XAU\n /XAG\n /XPT\n /XPD'''
 
-    bot.send_message(message.chat.id, answer)
-
-
-@bot.message_handler(content_types=["text"])
-def sendcurrency(message):
-    print("========================")
-    print(str(message.from_user.id))
-
-    with open("stat.txt", "a") as file:
-        file.write(str(message.chat.id)+","+ datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"\n")
-
-
-    bot.send_message(message.chat.id, get_rate(message.text))
+    bot.send_message(message.chat.id, lstcur)
 
 @bot.message_handler(content_types=["audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact"])
 def sendmessage(message):
     bot.send_message(message.chat.id, "This content is not supported. Please, specify a currency name in a right way: \n /USD or \n /EUR  or \n USD, EUR \n etc ")
-# audio, document, photo, sticker, video, video_note, voice, location, contact
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "other":
+       bot.send_message(call.from_user.id, lstcur)
+    else:
+        bot.send_message(call.from_user.id, get_rate(call.data))
+        stat(call.from_user.id)
+
+@bot.message_handler(content_types=["text"])
+def message_handler(message):
+    txt = message.text.replace("/", "").upper()
+    if txt in listofcurrencies:
+        bot.send_message(message.chat.id, get_rate(message.text) )
+    else:
+        bot.send_message(message.chat.id, "Please, select a  currency", reply_markup=gen_markup())
+
+    stat(message.chat.id)
 
 @app.route('/')
 def index():
     return 'Hello!'
-
